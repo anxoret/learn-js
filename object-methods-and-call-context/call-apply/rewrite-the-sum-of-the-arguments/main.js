361e92303342214486f6eab32d273a973a08776f
anxoret <post.lux@yandex.ru>
2019-05-16 14:31:36 +0300

    Решение задачи Перепишите суммирование аргументов

---
+++ object-methods-and-call-context/call-apply/rewrite-the-sum-of-the-arguments/main.js
@@ -0,0 +1,48 @@
+// вариант просто с "одалживанием метода"
+// function sumArgs() {
+//     // скопируем reduce из массива
+//     arguments.reduce = [].reduce;
+
+//     return arguments.reduce(function(a, b) {
+//       return a + b;
+//     });
+// };
+
+// alert( sumArgs(1, 2, 3) ); // 6, аргументы переданы через запятую, без массива
+
+
+// вариант решения через "одалживание метода" и call
+
+// function sumArgs() {
+
+//     let reduce = [].reduce;
+
+//     return reduce.call(arguments, function(a, b) {
+//         return a + b;
+//     });
+
+//     // либо ещё короче без сохранения [].reduce в переменную:
+//     // return [].reduce.call(arguments, function(a, b) {
+//     //     return a + b;
+//     // });
+
+// };
+  
+// alert( sumArgs(1, 2, 3) ); // 6, аргументы переданы через запятую, без массива
+
+// вариант решения через "одалживание метода" и apply 
+// (НЕПОНЯТНО, ПРАВИЛЬНЫЙ ЛИ МЕТОД РЕШЕНИЯ)
+
+function sumArgs() {
+
+    // arguments.reduce = [].reduce;
+    // либо просто:
+
+    return [].reduce.apply(arguments, [function(a, b) {
+        return a + b;
+    }]);
+
+};
+
+alert( sumArgs(1, 2, 3) ); // 6, аргументы переданы через запятую, без массива
+

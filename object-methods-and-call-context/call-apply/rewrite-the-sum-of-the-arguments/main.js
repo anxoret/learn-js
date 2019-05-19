c6364ba8906c0e7095609655eb8e585090474fe1
anxoret <post.lux@yandex.ru>
2019-05-19 19:42:31 +0300

    Добавлен ещё один вариант решения задачи

--- object-methods-and-call-context/call-apply/rewrite-the-sum-of-the-arguments/main.js
+++ object-methods-and-call-context/call-apply/rewrite-the-sum-of-the-arguments/main.js
@@ -44,5 +44,20 @@ alert( sumArgs(1, 2, 3) ); // 6, аргументы переданы через 
 
 // };
 
+// либо последний вариант вот так:
+// function sumArgs() {
+
+//   let arr = [].slice.call(arguments);
+
+//   reduce = [].reduce;
+
+
+
+//   return reduce.apply(arr, [function(a,b) {
+//       return a + b;
+//   }]);
+
+// }; 
+
 // alert( sumArgs(1, 2, 3) ); // 6, аргументы переданы через запятую, без массива
 

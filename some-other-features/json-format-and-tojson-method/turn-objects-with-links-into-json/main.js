41ed59cbe2abf9a614ff90d4b48314c0926175b1
anxoret <post.lux@yandex.ru>
2019-05-25 21:17:06 +0300

    Решение задачи Превратите объекты со ссылками в JSON

--- some-other-features/json-format-and-tojson-method/turn-objects-with-links-into-json/main.js
+++ some-other-features/json-format-and-tojson-method/turn-objects-with-links-into-json/main.js
@@ -1,41 +1,71 @@
-let leader = {
-    name: "Василий Иванович",
-    // soldier: soldier
-};
+// УСЛОВИЯ ЗАДАНИЯ
 
-let soldier = {
-    name: "Петька",
-    // leader: leader
-};
+// let leader = {
+//     name: "Василий Иванович",
+// };
+
+// let soldier = {
+//     name: "Петька",
+// };
 
 // эти объекты ссылаются друг на друга!
 // leader.soldier = soldier;
 // soldier.leader = leader;
 
-// итак, переходим на новый формат данных. начинаем хранить не
-// ссылки на объекты, а их ключи в виде строк (такое спокойно реализуется)
-// вместо leader.soldier = soldier;
-leader.soldier = "soldier";
-// вместо soldier.leader = leader;
-soldier.leader = "leader";
+// ПРАВИЛЬНЫЕ РЕШЕНИЯ ЗАДАЧИ: ЭТО ЛИБО ИСПОЛЬЗОВАНИЕ ФРЕЙМВОРКА Dojo И ЕГО использование 
+// особой реализации JSON, которая не входит в стандарт и 
+// поддерживает расширенный формат для поддержки ссылок
+// (А ИМЕННО ВЫЗОВ dojox.json.ref.toJson(team)), ЛИБО ЧТО 
+// ЕСТЬ ЛУЧШЕЕ РЕШЕНИЕ (!) использовать вместо ссылок id и 
+// измененная с помощью id структура будет выглядеть так:
 
-let team = [leader, soldier];
+let leader = {
+    id: 12,
+    name: "Василий Иванович"
+};
 
-// for (let i = 0; i < team.length; i++) {
+let soldier = {
+    id: 51,
+    name: "Петька"
+};
 
-//     if ({}.toString.call(team[i]) === "[object Object]") {
+// поменяли прямую ссылку на ID
+leader.soldierId = 51;
+soldier.leaderId = 12;
 
-//         for (let key in team[i]) {
-//             team[i][key] = team[i][key] + "";
-//             // alert(typeof team[i][key]);
+let team = {
+    12: leader,
+    51: soldier
+};
 
-//         };
+// а затем нужно сделать некую функцию, находящую по id объект
 
-//     } 
+// А ВСЕ РЕШЕНИЯ НИЖЕ - ЭТО НЕВЕРНЫЕ РЕШЕНИЯ! 
 
-// };
+// // итак, переходим на новый формат данных. начинаем хранить не
+// // ссылки на объекты, а их ключи в виде строк (такое спокойно реализуется)
+// // вместо leader.soldier = soldier;
+// leader.soldier = "soldier";
+// // вместо soldier.leader = leader;
+// soldier.leader = "leader";
 
-let result =  JSON.stringify(team);
-result = JSON.parse(result);
+// let team = [leader, soldier];
 
-alert(result[0]["soldier"]);
+// // for (let i = 0; i < team.length; i++) {
+
+// //     if ({}.toString.call(team[i]) === "[object Object]") {
+
+// //         for (let key in team[i]) {
+// //             team[i][key] = team[i][key] + "";
+// //             // alert(typeof team[i][key]);
+
+// //         };
+
+// //     } 
+
+// // };
+
+// let result =  JSON.stringify(team);
+// //result = JSON.parse(result);
+
+// alert(result);

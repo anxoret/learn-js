4a25709d1b7e61d253e46a75edefc5fc3baaebce
anxoret <post.lux@yandex.ru>
2019-06-05 15:26:24 +0300

    Решение задачи Добавить метод и свойство кофеварке

---
+++ oop-in-a-functional-style/internal-and-external-interface/add-a-method-and-property-to-a-coffee-maker/main.js
@@ -0,0 +1,33 @@
+function CoffeeMachine(power) {
+    this.waterAmount = 0;
+  
+    let WATER_HEAT_CAPACITY = 4200;
+  
+    let self = this;
+
+    let timerId;
+  
+    function getBoilTime() {
+        return self.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+    };
+  
+    function onReady() {
+        alert( 'Кофе готово!' );
+    };
+
+
+    this.run = function() {
+        timerId = setTimeout(onReady, getBoilTime());
+    };
+
+    this.stop = function() {
+        clearTimeout(timerId);
+    };
+  
+  };
+
+    let coffeeMachine = new CoffeeMachine(50000);
+    coffeeMachine.waterAmount = 200;
+
+    coffeeMachine.run();
+    coffeeMachine.stop(); // кофе приготовлен не будет

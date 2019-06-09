d6cec4bd389b61a2a671897c93938e73b28741ae
anxoret <post.lux@yandex.ru>
2019-06-09 14:12:55 +0300

    Решение задачи Переопределите disable

---
+++ oop-in-a-functional-style/functional-inheritance/redefine-disable/main.js
@@ -0,0 +1,79 @@
+function Machine(power) {
+    this._power = power;
+    this._enabled = false;
+
+    // let self = this;
+
+    this.enable = function() {
+      this._enabled = true;
+    };
+
+    this.disable = function() {
+      this._enabled = false;
+    };
+};
+
+function Fridge(power) {
+    Machine.apply(this, arguments);
+
+    let food = []; // создали приватное свойство
+
+    this._capacity = this._power / 100;
+
+    let parentDisable = this.disable;
+
+    this.disable = function() {
+        if  (food != []) {
+            throw new Error ("В холодильнике есть еда, поэтому его нельзя выключить.")
+        };
+        parentDisable();
+    };
+ 
+    this.addFood = function(item) {
+
+        if (!this._enabled /*= false*/ ) {
+            throw new Error ("Холодильник выключен. Вы не можете добавить еду.");
+        };
+
+        let foodToAdd = [].slice.call(arguments);
+
+        for (let i = 0; i < foodToAdd.length; i++) {
+
+            if ( (food.length + 1) > this._capacity) {
+                throw new Error ("Холодильник переполнен. Вы не можете добавить еду.");
+            };
+
+            food.push(foodToAdd[i]);
+
+        };
+
+    };
+
+    this.getFood = function() {
+        return food.slice();
+    };
+
+    this.filterFood = function(func) {
+      return food.filter(func);
+    };
+
+    this.removeFood = function(item) {
+        for (let i = 0; i < food.length; i++) {
+            if (food[i].title == item.title) {
+                food.splice(i, 1);
+                i--;
+            };
+        };
+
+        // либо короткий вариант по Кантору:
+        // let idx = food.indexOf(item);
+        // if (idx != -1) food.splice(idx, 1);
+    };
+
+};
+
+let fridge = new Fridge(500);
+fridge.enable();
+fridge.addFood("кус-кус");
+fridge.disable(); // ошибка, в холодильнике есть еда
+

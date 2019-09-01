7a35a510255e8b7d923c2402962deb9ee4318a13
anxoret <post.lux@yandex.ru>
2019-09-01 14:05:30 +0300

    Решение задачи Перепишите, используя async/await lvl 2

---
+++ es-2015/promises-async-await/promises/async-await/rewrite-using-async-await-lvl-2/before-rewrite.js
@@ -0,0 +1,39 @@
+class HttpError extends Error {
+    constructor(response) {
+      super(`${response.status} for ${response.url}`);
+      this.name = 'HttpError';
+      this.response = response;
+    }
+  }
+  
+  function loadJson(url) {
+    return fetch(url)
+      .then(response => {
+        if (response.status == 200) {
+          return response.json();
+        } else {
+          throw new HttpError(response);
+        }
+      })
+  }
+  
+  // Запрашивать логин, пока github не вернёт существующего пользователя.
+  function demoGithubUser() {
+    let name = prompt("Введите логин?", "iliakan");
+  
+    return loadJson(`https://api.github.com/users/${name}`)
+      .then(user => {
+        alert(`Полное имя: ${user.name}.`);
+        return user;
+      })
+      .catch(err => {
+        if (err instanceof HttpError && err.response.status == 404) {
+          alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+          return demoGithubUser();
+        } else {
+          throw err;
+        }
+      });
+  }
+  
+  demoGithubUser();

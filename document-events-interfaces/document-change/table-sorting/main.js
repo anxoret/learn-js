36104c6d9062c493c6bd99f04d64a7dc9337e9c6
anxoret <post.lux@yandex.ru>
2019-09-20 15:53:38 +0300

    Making changes in solution of the task

--- document-events-interfaces/document-change/table-sorting/main.js
+++ document-events-interfaces/document-change/table-sorting/main.js
@@ -1,17 +1,3 @@
-// function sortStrings(a, b) {
-//     if (a.innerText > b.innerText) return 1;
-//     if (a.innerText == b.innerText) return 0;
-//     if (a.innerText < b.innerText) return -1;
-// };
-
-// let arrayOfNamesInTable = document.querySelectorAll(".name");
-// // let arrayOfNamesInTable = [1, 0, 2];
-// arrayOfNamesInTable.sort(sortStrings);
-
-// // alert(arrayOfNamesInTable);
-
-// // console.info(arrayOfNamesInTable);
-
 let sortedRows = Array.from(table.rows)
     .slice(1)
     .sort((trA, trB) => 
